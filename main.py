167c808aa9ed157bcdd7350041f6278b3fa262ee
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-08-24 22:03:33 +0530

    Hmm (c) @Alone_loverboy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import os
-os.system("git clone https://github.com/Copycat-izz-opp/Copycat_Session_Hack_Bot")
-os.system("cd Copycat_Session_Hack_Bot && python bot.py")
+from os import system
+
+system(
+      "git clone https://github.com/Copycat-izz-opp/Copycat_Session_Hack_Bot && cd Copycat_Session_Hack_Bot && python3 -m bot.py"
+)
