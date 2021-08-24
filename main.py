6cd35c271fadbbb669e08225aaab7f2c6270f47f
Copycat-izz-opp <85601169+Copycat-izz-opp@users.noreply.github.com>
2021-08-24 21:54:33 +0530

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
 os.system("git clone https://github.com/Copycat-izz-opp/Copycat_Session_Hack_Bot")
-os.system("cd sessionhack_bot && python bot.py")
+os.system("cd Copycat_Session_Hack_Bot && python bot.py")
